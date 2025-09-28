36a7a7866753488b025722c03c36ab6d2e25bd29
IoaBa86 <ioannisbakas86@gmail.com>
2025-09-28 16:26:42 +0300

    google adsense fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,15 +85,14 @@ export default function RootLayout({
         
         {/* Theme color */}
         <meta name="theme-color" content="#8B5A3C" />
-
-        {/* Google AdSense Script - Fixed */}
-        <Script
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1184028367307988"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
       </head>
       <body className={`${inter.className} antialiased`}>
+        {/* Google AdSense Script - Moved outside head to fix warning */}
+        <Script
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1184028367307988"
+          strategy="beforeInteractive"
+        />
+        
         <Providers>
           <div className="flex flex-col min-h-screen">
             <Header />
